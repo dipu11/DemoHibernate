43a4e9c5334dfff74ef97da3525bf2244a78b2cc
dipu11 <dipubuet2@gmail.com>
2019-05-19 21:52:16 +0600

    git fetch demo

--- src/main/java/com/hibernatetest/DemoHibernate/App.java
+++ src/main/java/com/hibernatetest/DemoHibernate/App.java
@@ -6,11 +6,10 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
-
 /**
- * Hello world!
- *
+ * @author DIPU
  */
+
 public class App 
 {
     public static void main( String[] args )
@@ -18,9 +17,9 @@ public class App
         System.out.println( "Hello World!" );
         
         Allien allien= new Allien();
-        allien.setAid(102);
-        allien.setName("testname3");
-        allien.setColor("red"); 
+       /* allien.setAid(103);
+        allien.setName("testname33");
+        allien.setColor("red"); */
         
         Configuration conf= new Configuration().configure().addAnnotatedClass(Allien.class);
         
@@ -29,8 +28,10 @@ public class App
         SessionFactory sf= conf.buildSessionFactory(reg); 
         Session session= sf.openSession();
         Transaction tx= session.beginTransaction();
+        allien= (Allien)session.get(Allien.class, 101);
         
-        session.save(allien);
+      //  session.save(allien);
         tx.commit();
+        System.out.println(allien); 
     }
 }
