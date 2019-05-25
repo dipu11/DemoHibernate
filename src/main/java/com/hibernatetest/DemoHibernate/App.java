03c4c5312d6339d5d3a059e1c69939eeef8d1d46
dipu11 <dipubuet2@gmail.com>
2019-05-25 23:47:24 +0600

    fetching row/objects by different HQL example

--- src/main/java/com/hibernatetest/DemoHibernate/App.java
+++ src/main/java/com/hibernatetest/DemoHibernate/App.java
@@ -1,7 +1,9 @@
 package com.hibernatetest.DemoHibernate;
 
+import java.util.List;
 import java.util.Random;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -27,7 +29,8 @@ public class App
         Session session= sf.openSession();
         Transaction tx= session.beginTransaction();
       
-        Random rand= new Random();
+        //save objects:
+      /*  Random rand= new Random();
         
         for(int i=1;i<=50;i++)
         {
@@ -37,8 +40,29 @@ public class App
         	stuent.setMarks(rand.nextInt(100)); 
         	
         	session.save(stuent); 
+        }*/
+        
+        //now fetch list of objects:
+        Query query= session.createQuery("from Student");
+        List<Student>list= query.list(); 
+        
+        for(Student student: list)
+        {
+        	System.out.println(student); 
         }
         
+        //Query with conditions: where clause
+        Query query2= session.createQuery("from Student where marks>=50");
+        List<Student> list2= query2.list();
+        for(Student student: list2)
+        {
+        	System.out.println(student); 
+        }
+        
+        //Query for single, specific row:
+        Query query3= session.createQuery("from Student where rollno=7");
+        Student student= (Student)query3.uniqueResult();
+        System.out.println(student); 
         
         session.getTransaction().commit();
         System.out.println();  
